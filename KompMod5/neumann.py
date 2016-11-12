dd3f80a0f035de935bf851cee627d052266f0c9f
Murddraal <2dents@mail.ru>
2016-11-12 17:34:19 +0700

    исправлено вычисление границ и нахождение M

--- KompMod5/neumann.py
+++ KompMod5/neumann.py
@@ -3,20 +3,27 @@ import scipy.stats
 import normal_distrib as n_d
 import random
 import numpy as np
+import math
 
 def bounds(u, v):
     
-    a = int(scipy.stats.norm.ppf(0.01, u, v))
-    b = int(scipy.stats.norm.ppf(0.99, u, v))
+    a = (scipy.stats.norm.ppf(0.01, u, v))
+    b = (scipy.stats.norm.ppf(0.99, u, v))
 
     return a, b
 
 def generate(u, v):
     a, b = bounds(u, v)
     arr_M = []
-    for x in range(a, b+1):
-        arr_M.append(n_d.pdf(x, u, v))
-    M = max(arr_M)
+    step = (b - a) / 100
+    i = a
+    #for x in range(a, b+1):
+    #    arr_M.append(n_d.pdf(x, u, v))
+    #M = max(arr_M)
+    while i < b :
+        arr_M.append(n_d.pdf(i, u, v))
+        i += step
+    M = max(arr_M)        
 
     x = a + random.random() * (b - a)
     y = random.random() * M
